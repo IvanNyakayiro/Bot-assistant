c22a7877505d6e81913ec271700dc253d5b3c44b
Ivan Nyakayiro <43791764+IvanNyakayiro@users.noreply.github.com>
2018-10-05 21:51:33 -0400

    update index.js
    Added new functions

--- index.js
+++ index.js
@@ -10,7 +10,39 @@ bot.on("ready", async() =>{
 
 });
 
+bot.on("message", (message) =>{
+	if(message.content.startsWith("!ping")){
+		message.channel.send("pong!");
+	}
+	if(message.content.startsWith("!hello")){
+		message.channel.send("Hello!");
+	}
+	if(message.content.startsWith("!hey")){
+		message.channel.send("hey!");
+	}
+	if(message.content.startsWith("!help")){
+		message.channel.send("How can I help!");
+	}
 
+});
+
+bot.on("guildMemberAdd", (member) =>{
+	const guild = member.guild;
+	if(!newUsers[guild.id]) newUsers[guild.id] = new Discord.Collection();
+		newUsers[guild.id].set(member.id, member.user);
+
+		if(newUsers[guild.id].size > 10){
+			const userlist = newUsers[guild.id].map(u => u.toString()).join(" ");
+    	guild.channels.get(guild.id).send("Welcome our new users!\n" + userlist);
+    	newUsers[guild.id].clear();
+		}
+});
+
+bot.on("guildMemberRemove", (member) => {
+  const guild = member.guild;
+  if (newUsers[guild.id].has(member.id)) newUsers.delete(member.id);
+});
+/*
 bot.on("message",async message =>{
 	if(message.author.bot) return;
 	if (message.chanel.type === "dm") return;
@@ -25,5 +57,5 @@ bot.on("message",async message =>{
 	}
 
 });
-
+*/
 bot.login(botconfig.token);
